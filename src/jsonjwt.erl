439c666103c4bee11dcf3c3049f331f403b42499
Ryan Huffman <ryanhuffman@gmail.com>
2012-01-31 21:34:23 -0800

    Add unicode and more algorithm suppor to jsonjwt

--- src/jsonjwt.erl
+++ src/jsonjwt.erl
@@ -14,24 +14,33 @@
 
 -type claim() :: {string(), string() | integer() | atom()}.
 
+-type crypto_algorithm() :: "HS256" | "HS384" | "HS512" | "none".
+
 %% @doc Encodes a JWT.  Algorithm should be "HS256" or "none".
 -spec encode([claim()], string(), string()) -> string().
 encode(Claims, Key, Algorithm) ->
     Header = [{<<"typ">>, <<"JWT">>}, {<<"alg">>, list_to_binary(Algorithm)}],
-    HeaderJSON = iolist_to_binary(mochijson2:encode({struct, Header})),
+    HeaderJSON = unicode:characters_to_binary(mochijson2:encode({struct, Header}), utf8),
     HeaderEncoded = base64:encode(HeaderJSON),
 
-    MessageJSON = iolist_to_binary(mochijson2:encode({struct, Claims})),
+    MessageJSON = unicode:characters_to_binary(mochijson2:encode({struct, Claims}), utf8),
     MessageEncoded = base64:encode(MessageJSON),
 
-    case Algorithm of
-        "none" ->
-            Signature = <<"">>;
-        _ ->
-            SigningInput = <<HeaderEncoded/binary, ".", MessageEncoded/binary>>,
-            Signature = hmac:hmac256(Key, SigningInput)
-    end,
+    SigningInput = <<HeaderEncoded/binary, ".", MessageEncoded/binary>>,
+    Signature = sign(Algorithm, Key, SigningInput),
 
     SignatureEncoded = base64:encode(Signature),
     <<HeaderEncoded/binary, ".", MessageEncoded/binary, ".", SignatureEncoded/binary>>.
+
+sign("none", _Key, _Data) ->
+    <<"">>;
+sign("HS256", Key, Data) ->
+    hmac:hmac256(Key, Data);
+sign("HS384", Key, Data) ->
+    hmac:hmac384(Key, Data);
+sign("HS512", Key, Data) ->
+    hmac:hmac512(Key, Data);
+sign(_Algorithm, _Key, _Data) ->
+    throw(unsupported_algorithm).
+
 
